782606a4731078f41d2e7aae18304280a0b3a732
vnskingmotor <57930370+VnsKing001@users.noreply.github.com>
2022-06-18 21:06:27 +0700

    git commit -m configMicro

--- project/udagram-api/src/config/config.ts
+++ project/udagram-api/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': "postgres",
+  'password': "VnsKing7899",
+  'database': "vnskingdb",
+  'host': "mydatabase-instance.csnltkjxydxi.us-east-1.rds.amazonaws.com",
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': "us-east-1",
+  'aws_profile': "default",
+  'aws_media_bucket': "elasticbeanstalk-us-east-1-643327991368",
+  'url': "http://localhost:8100",
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': "testing",
   },
 };
